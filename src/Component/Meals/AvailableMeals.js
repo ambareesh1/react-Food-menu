92733d4faf19052a8f6dbeccd4855fcc2dd111ab
ambareesh1 <ambru333@gmail.com>
2022-08-15 20:58:48 +0530

    2 files modified, 4 added, 1 deleted

--- src/Component/Meals/AvailableMeals.js
+++ src/Component/Meals/AvailableMeals.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classes from './AvailableMeals.module.css';
-
+import Card from '../UI/Card';
 const DUMMY_MEALS = [
   {
     id: 'm1',
@@ -32,7 +32,9 @@ const AvailableMeals = (props) => {
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
--- src/Component/Meals/Meals.js
+++ src/Component/Meals/Meals.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 
-import MealsSummary from './MealsSUmmary';
+import MealsSummary from './MealsSummary';
 import AvailableMeals from './AvailableMeals';
 
 const Meals = (props) => {
